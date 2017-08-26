1ce14a0d1b67497ba4eda43e38e8136e7a9e2740
monodyle <maclucphamdev@gmail.com>
2017-08-26 22:11:09 +0700

    Fixed error. Version 1.2

---
+++ Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AgeKnow
+{
+    partial class AgeKnow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgeKnow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.hightval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.help = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your height";
+            // 
+            // hightval
+            // 
+            this.hightval.Location = new System.Drawing.Point(105, 19);
+            this.hightval.MaxLength = 3;
+            this.hightval.Name = "hightval";
+            this.hightval.Size = new System.Drawing.Size(42, 20);
+            this.hightval.TabIndex = 1;
+            this.hightval.KeyUp += new System.Windows.Forms.KeyEventHandler(this.hightval_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "cm";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(67, 60);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(90, 24);
+            this.submit.TabIndex = 2;
+            this.submit.Text = "Result";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // help
+            // 
+            this.help.AccessibleName = "MainBox";
+            this.help.Location = new System.Drawing.Point(31, 60);
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(24, 24);
+            this.help.TabIndex = 2;
+            this.help.Text = "?";
+            this.help.UseVisualStyleBackColor = true;
+            this.help.Click += new System.EventHandler(this.help_Click);
+            // 
+            // AgeKnow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 101);
+            this.Controls.Add(this.help);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.hightval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AgeKnow";
+            this.ShowInTaskbar = false;
+            this.Text = "AgeKnow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox hightval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Button help;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeKnow
+{
+    public partial class AgeKnow : Form
+    {
+        public AgeKnow()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("{TAB}");
+            // check numberic
+            int n;
+            string height = hightval.Text;
+            bool isNumeric = int.TryParse(height, out n);
+            if (isNumeric)
+            {
+                // true number
+                int val = Int32.Parse(height);
+                if (val < 50)
+                {
+                    //MessageBox.Show("Bạn xạo lìn vãi, có thằng nào thấp dưới 50cm?", "Xạo lòn quen", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("You lie, who smaller than 50cm?", "Why you lie me?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (val > 220)
+                {
+                    //MessageBox.Show("Bạn xạo lìn vãi, có thằng Việt Nam nào cao trên 200cm?", "Xạo lòn quen", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("You lie, who hight more than 220cm?", "Lie?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    //MessageBox.Show("Chiều cao của bạn là " + height + "cm!", "Kết quả", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    string trueh = "";
+                    if ((val / 100) < 1)
+                    {
+                        trueh = (val % 100) + "cm";
+                    }
+                    else
+                    {
+                        trueh = (val / 100) + "m" + (val % 100);
+                    }
+                    //MessageBox.Show("Bạn cao " + trueh + "!", "Kết quả", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("You hight " + trueh + "!", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (height == "")
+            {
+                //MessageBox.Show("Dữ liệu trống không. Vui lòng nhập số vào.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Your field blank. Please enter a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //MessageBox.Show("Bạn nên nhập số, không phải chữ", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("You must type number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void hightval_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                submit.PerformClick();
+            }
+        }
+
+        private void help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("seriously? dude, I worry about your reading skill.\n\njust type your hight (as `cm` unit), and click `Result`.\n\nGot it? do it now.", "My god!", MessageBoxButtons.OK);
+        }
+    }
+}
